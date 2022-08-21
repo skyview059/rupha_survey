469c6afbd4e69c0155fcb9c744dba15770b9d0dd
Iqbal Hossen <iqbalhossen999@gmail.com>
2022-08-21 09:56:31 -0400

    Somthing changes

--- crud/core/connection.php
+++ crud/core/connection.php
@@ -1,28 +1,34 @@
 <?php
 
 
-$active_group = 'default';
-$query_builder = TRUE;
+$active_group   = 'default';
+$query_builder  = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'arch_vista_light',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'dsn'      => '',
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'database' => 'rupsa_survey',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
 
+
+/* For only Keeping DB Backup Logs */
+$db['sqlite'] = array(
+    'dsn'      => 'sqlite:DB/backup_logs.sqlite',   // path/to/database
+    'dbdriver' => 'pdo'
+);
