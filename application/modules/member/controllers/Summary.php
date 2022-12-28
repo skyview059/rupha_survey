d61f1cb29f8c1d853b23e1773cdee6d8c7b2405c
Khairul Azam <skyview059@gmail.com>
2022-12-28 13:55:21 +0600

    Updates of Member

--- application/modules/member/controllers/Summary.php
+++ application/modules/member/controllers/Summary.php
@@ -17,8 +17,37 @@ class Summary extends Admin_controller {
     public function index()
     {
 
-        $data = [];
-        $this->viewAdminContent('member/member/summary', $data);
+        
+    
+        $today = date( 'Y-m-d');
+        $last7Days = date('Y-m-d', strtotime("-7 day"));
+        $statistics = [];
+        $secretaries = $this->db->where_in('role_id',[3,4])->where('status', 'Active')->get('users')->result();
+        if($secretaries){
+            foreach ($secretaries as $user) {
+
+                //Get union information by ID
+                $union_info = $this->Member_model->getUnionInfoById($user->union_id);
+
+                //Get Member Statistics Data
+                $todayCount = $this->db->where('created_by', $user->id)->where('DATE(created_at)', $today)->count_all_results('members');
+                $last7DayCount = $this->db->where('created_by', $user->id)->where('DATE(created_at)>=', $last7Days)->where('DATE(created_at)<=', $today)->count_all_results('members');
+                $currentMonthCount = $this->db->where('created_by', $user->id)->where('MONTH(created_at)', date("m"))->count_all_results('members');
+                $currentYearCount = $this->db->where('created_by', $user->id)->where('YEAR(created_at)', date("Y"))->count_all_results('members');
+                $lifetimeCount = $this->db->where('created_by', $user->id)->count_all_results('members');
+
+                $statistics[$user->id]['full_name'] = $user->full_name;
+                $statistics[$user->id]['union_info'] = $union_info;
+                $statistics[$user->id]['today_count'] = $todayCount;
+                $statistics[$user->id]['last_7_day_count'] = $last7DayCount;
+                $statistics[$user->id]['current_month_count'] = $currentMonthCount;
+                $statistics[$user->id]['current_year_count'] = $currentYearCount;
+                $statistics[$user->id]['lifetime_count'] = $lifetimeCount;
+            }
+        }        
+        
+//        dd( $statistics );
+        $this->viewAdminContent('member/member/summary', $statistics );
     }
     
 }
--- application/modules/member/views/member/summary.php
+++ application/modules/member/views/member/summary.php
@@ -10,15 +10,52 @@
 </section>
 
 <section class="content">    
-    <div class="box no-border">
+
+    <div class="box box-primary">
         <div class="box-header with-border">
-            <h3 class="box-title">বিস্তারিত দেখুন</h3>            
+            <h3 class="box-title">Member Statistics</h3>
         </div>
         <div class="box-body">
-            
+            <div class="table-responsive">
+                <table class="table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Sectary Name</th>
+                            <th>Union</th>
+                            <th class="text-center">Today</th>
+                            <th class="text-center">Last 7 Days</th>
+                            <th class="text-center">Current Month</th>
+                            <th class="text-center">Current Year</th>
+                            <th class="text-center">Lifetime</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                        if (!empty($statistics)) {
+                            foreach ($statistics as $userId => $user) {
+                                ?>
+                                <tr>
+                                    <td><?php echo anchor(site_url(Backend_URL . 'member?user_id=' . $userId), $user['full_name'], 'class=""'); ?></td>
+                                    <td>
+                                        <h5><?= $user['union_info']->union_bn_name; ?> ইউনিয়ন পরিষদ</h5>
+                                        <p>উপজেলা: <?= $user['union_info']->upazila_bn_name; ?>, <?= $user['union_info']->district_bn_name; ?>, <?= $user['union_info']->division_bn_name; ?></p>
+                                    </td>
+                                    <td class="text-center"><?= $user['today_count'] ?></td>
+                                    <td class="text-center"><?= $user['last_7_day_count'] ?></td>
+                                    <td class="text-center"><?= $user['current_month_count'] ?></td>
+                                    <td class="text-center"><?= $user['current_year_count'] ?></td>
+                                    <td class="text-center"><?= $user['lifetime_count'] ?></td>
+                                </tr>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="box-footer">
-            
-        </div>
+        <div class="box-footer"></div>
     </div>
+
 </section>
